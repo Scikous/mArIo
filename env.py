16386121ff36a52f8c9aea814d67daa4020b1b16
Scikous <95752605+Scikous@users.noreply.github.com>
2024-03-09 16:24:56 +0200

    WIP DeepQLearning, Moved env wrappers to utils, Created frames skipping method, and memory with random sampling for statesother minor improvements

--- env.py
+++ env.py
@@ -1,30 +1,41 @@
 from nes_py.wrappers import JoypadSpace
 import gym_super_mario_bros
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
-from gym.wrappers import FrameStack, GrayScaleObservation, ResizeObservation
+from model import Network
+import agent
+import utils
+
 env = gym_super_mario_bros.make('SuperMarioBros-v0')
 env = JoypadSpace(env, SIMPLE_MOVEMENT)
-from model import cnn
-import torch
-import numpy as np
+env = utils.EnvUtils.wrapper(env)
 
-# Optionally apply wrappers in the desired order:
-env = GrayScaleObservation(env)    # Convert to grayscale
-env = ResizeObservation(env, shape=(84, 84)) 
-env = FrameStack(env, num_stack=4)  # Stack 4 frames
-obs = env
+
+prev_state = env.reset()
 print(env.observation_space.shape)
-test = cnn(env.observation_space.shape, len(SIMPLE_MOVEMENT))
 done = True
-# for step in range(10000):  # 5000 steps max, you can change this to any number you want
-#     if done:
-#         state = env.reset()
-#     action = env.action_space.sample()  # take a random action
-#     state, reward, done, info = env.step(action)
-#     #state = torch.tensor()
-#     d = torch.from_numpy((np.array(state))).unsqueeze(0).float().squeeze(-1)
-#     print(info)#test(d))
-#     env.render()  # display the environment state
-#     if done:
-#         break
+
+batch_size = 32
+replay_memory = utils.ReplayMem(max_size=500)
+test = Network(env.observation_space.shape, len(SIMPLE_MOVEMENT), batch_size)
+
+
+
+for step in range(10000):  # 5000 steps max, you can change this to any number you want
+    if done:
+        state = env.reset()
+    action = env.action_space.sample()  # take a random action
+    next_state, reward, done, info = env.step(action)
+    experience = (state, action, reward, next_state, done)
+    replay_memory.append(experience)
+    state = next_state
+    if replay_memory.size >= batch_size:
+        states, actions, rewards, next_states, dones = replay_memory.random_sample(batch_size=batch_size) 
+    #print(len(states))
+        if states is not None:
+            test(states)
+    #print(states)
+
+    env.render()  # display the environment state
+    if done:
+        break
 env.close()
---
+++ utils.py
@@ -0,0 +1,53 @@
+import numpy as np
+import random
+import torch
+from collections import deque
+from gym.wrappers import FrameStack, GrayScaleObservation, ResizeObservation
+
+
+class ReplayMem:
+    def __init__(self, max_size):
+        self.replay_memory = deque(maxlen=max_size)
+
+    @property
+    def size(self):#knowing of the taken memory, very useful
+        return len(self.replay_memory)
+    #add experience to memory
+    def append(self, experience):
+        self.replay_memory.append(experience)
+
+    #get a batch of random experiences
+    def random_sample(self, batch_size):
+        if batch_size > len(self.replay_memory):
+            return None
+        experiences = random.sample(self.replay_memory, batch_size) 
+        states, actions, rewards, next_states, dones = zip(*experiences)  # Unpack experience tuples
+
+        # Convert all elements to tensors using list comprehension, numpy and torch.tensor
+        states = torch.tensor(np.asarray([state for state in states]), dtype=torch.float32).squeeze(-1)
+        actions = torch.tensor(np.asarray([action for action in actions]), dtype=torch.float32).unsqueeze(-1)  # Assuming actions are continuous
+        rewards = torch.tensor(np.asarray([reward for reward in rewards]), dtype=torch.float32).unsqueeze(-1)
+        next_states = torch.tensor(np.asarray([next_state for next_state in next_states]), dtype=torch.float32).unsqueeze(-1)
+        dones = torch.tensor(np.asarray([done for done in dones]), dtype=torch.float32).unsqueeze(-1)
+
+
+        return states, actions, rewards, next_states, dones
+    
+
+class EnvUtils:
+    @staticmethod
+    def wrapper(env):
+        env = GrayScaleObservation(env)    # Convert to grayscale
+        env = ResizeObservation(env, shape=(84, 84)) 
+        env = FrameStack(env, num_stack=4)
+        return env
+    
+    @staticmethod
+    def frames_skipper(env, action, skip_frames=4):
+        total_reward = 0.0
+        for _ in range(skip_frames):
+            next_state, reward, done, info = env.step(action)
+            total_reward += reward
+            if done:
+                break
+        return next_state, total_reward, done, info 
