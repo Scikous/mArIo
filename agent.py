Fixed new best model never being saved due to total average reward being miscalculated

agent.py
@@ -67,7 +67,7 @@ class Agent:
             try:
                 eps_rewards = np.load("episodes_rewards.npy")
                 eps_rewards = np.append(eps_rewards, episodes_reward)
-                total_average_reward = np.sum(eps_rewards)
+                total_average_reward = np.sum(eps_rewards)/len(eps_rewards)
             except Exception as e:
                 print(e)
                 eps_rewards = np.array([episodes_reward])

env.py
@@ -18,7 +18,8 @@ replay_memory = utils.ReplayMem(max_size=50000)
 mArIo = Agent(env.observation_space.shape, len(SIMPLE_MOVEMENT), batch_size)
 episodes_rewards = np.empty([0])
 
-utils.EnvUtils.episodes_rewards_plotter(mArIo.load_episodes_rewards())
+#plot accumalted rewards
+#utils.EnvUtils.episodes_rewards_plotter(mArIo.load_episodes_rewards())
 
 for episode in range(EPISODES):  # 5000 steps max, you can change this to any number you want
     done = False